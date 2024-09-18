be1738c92ea0d7dd1cbc741c1fadddf1da7a23af
vi-notych <vin.arnet@gmail.com>
2024-09-18 12:14:18 +0300

    внес правки в адаптив index

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,14 @@
 // Получаем элементы
 const navButton = document.querySelector('.header__nav-btn');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const headerNav = document.querySelector('.header__nav');
 // Добавляем обработчик события на кнопку
 navButton.addEventListener('click', function () {
   // Переключаем класс .header--active у элемента .header
-  header.classList.toggle('header--active');
+  body.classList.toggle('no-scroll');
   headerNav.classList.toggle('header__nav--active');
+  header.classList.toggle('header--active');
 });
 
 
